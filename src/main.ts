ba15cde8cb57f1c21f9bb5421835a77a0dddb968
ChakritDev <Chakrit-tom@hotmail.com>
2021-07-26 15:32:51 +0700

    add enableCors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); //อนุญาตทุกคนเข้ามาใช้
   app.setGlobalPrefix('/v1/api'); //loacalhost:3000/v1/api
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 3000);
